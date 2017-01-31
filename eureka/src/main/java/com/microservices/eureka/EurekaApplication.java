190263bd56a13a2128116c0ee922eb17a2912122
maksym.shalak <maksym.shalak@edvantis.com>
2017-01-31 16:12:19 +0200

    Added Eureka service discovery

--- eureka/src/main/java/com/microservices/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/microservices/eureka/EurekaApplication.java
@@ -1,18 +1,18 @@
-package com.microservices.exchange.rate;
+package com.microservices.eureka;
 
 import java.util.Locale;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-//@EnableEurekaClient
-public class ExchangeRateApplication
+@EnableEurekaServer
+public class EurekaApplication
 {
 	public static void main(String[] args)
 	{
 		Locale.setDefault(Locale.US);
-		SpringApplication.run(ExchangeRateApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
