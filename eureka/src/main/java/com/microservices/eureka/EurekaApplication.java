5a73832c8c0b267a56feb9bbab4bcc941526a0b5
maksshal@gmail.com <maksshal@gmail.com>
2018-12-03 17:28:10 +0200

    Removed Eclipse generated files

--- eureka/src/main/java/com/microservices/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/microservices/eureka/EurekaApplication.java
@@ -1,7 +1,5 @@
 package com.microservices.eureka;
 
-import java.util.Locale;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -12,7 +10,6 @@ public class EurekaApplication
 {
 	public static void main(String[] args)
 	{
-		Locale.setDefault(Locale.US);
 		SpringApplication.run(EurekaApplication.class, args);
 	}
 }
